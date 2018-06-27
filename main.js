cfab87508ecbb1f095eeee8aee4bc840d2b3dc08
Amenoimi <bmpss92285@gmail.com>
2018-06-27 17:41:58 +0800

    草動畫

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var canvas;
-var rain = new Array(200);
+var rain = new Array(100);
+var grass=new Array(20);
+
 
 function canvas_resize(){
   var wx = window.innerWidth;
@@ -12,11 +14,32 @@ function canvas_resize(){
 function draw_bk() {
     if (canvas.getContext) {
       var ctx = canvas.getContext("2d");
-      ctx.fillStyle = "rgb(50,50,200)";
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(10,100,255)";
+      
       ctx.fillRect (0, 0, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight)
-      ctx.fillStyle = "rgb(0,180,0)";
+      ctx.closePath()
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(10,90,250)";
+      ctx.fillRect (0, 0, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/2)
+      ctx.closePath()
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(10,80,245)";
+      ctx.fillRect (0, 0, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/3)
+      ctx.closePath()
+      ctx.beginPath();
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(10,70,240)";
+      ctx.fillRect (0, 0, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/4)
+      ctx.closePath()
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(10,60,255)";
+      ctx.fillRect (0, 0, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/5)
+      ctx.closePath()
+      //ctx.fillRect (0, 0, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/5*3)
+     // ctx.fillStyle = "rgb(0,180,0)";
       //ctx.fill();
-      ctx.fillRect (0, 400, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/2);
+      //ctx.fillRect (0,  document.getElementById("main").clientHeight/5*3, document.getElementById("main").clientWidth, document.getElementById("main").clientHeight/5*2);
 
     
     }
@@ -45,7 +68,7 @@ function rain_re(){
         x: window_w_rnd(),
         y: rnd(-500,0),
         h:60,
-        w:1,
+        w:3,
         radius: 3,
         color: "rgba(0,80,255,0.8)",
         draw: function() {
@@ -63,7 +86,13 @@ function rain_re(){
           ctx.bezierCurveTo(this.x - this.w / 4, this.y + this.h / 4, this.x - this.w / 2, this.y - this.h / 2, this.x, this.y - this.h / 2);
           ctx.fillStyle = "rgba(200,200,255,0.8)";
           ctx.fill();
-          ctx.closePath()
+          ctx.closePath();
+         // ctx.beginPath();
+         // var img =new Image();
+          //img.src="w.png";
+          //ctx.drawImage(img,0,0,document.getElementById("main").clientWidth,document.getElementById("main").clientHeight);
+          //ctx.closePath();
+
         }
       };
      
@@ -90,28 +119,74 @@ function rain_ing(){
 function grass_re(){
   if (canvas.getContext) {
     var ctx = canvas.getContext("2d");
-    ctx.quadraticCurveTo(300,100,500,120);  
+   
+    for (var i=0;i<grass.length;i++)
+    {
+      grass[i]={
+        x:rnd(0,document.getElementById("main").clientWidth) ,
+        y: rnd(document.getElementById("main").clientHeight-360,document.getElementById("main").clientHeight-300),
+        img:new Image(),
+        width:400,
+        height:500,
+        r:0,
+        color: "rgba(0,255,55,1)",
+        draw: function() {
+         
+          this.img.src="s.png";
+          ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+          ctx.rotate(this.r*Math.PI/180);
+         
+         
+        }
+      };
+     
+      grass[i].draw();
+    }
   }
 }
+function grass_ing(){
+  for (var i=0;i<grass.length;i++)
+  {
+    grass[i].width+=rnd(-12,12);
+    //grass[i].r=rnd(-0.5,0.5);
+    grass[i].draw();
+    
+  }
 
+}
+function grass_stop(){
+  for (var i=0;i<grass.length;i++)
+  {
+   
+    grass[i].draw();
+    
+  }
+
+}
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
   canvas = document.getElementById("main");
   canvas_resize();
-  rain_re()
-  //rain_ing();
-
+  rain_re();
+  grass_re();
   setInterval(function(){
+    canvas_resize();
     draw_bk();
+   // grass_ing();
+   grass_stop()
     rain_ing();
-    grass_re()
+    
   }, 50)
+  setInterval(function(){
+    grass_ing();
+  }, 200)
 });
 
 
 $(window).resize(function() {
   canvas_resize();
+  rain_re();
 });
 
 
